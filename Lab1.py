3900df6bc4a66b2a3e1da0786425de89d2feab93
GebbertOsermann <snowplow1310@gmail.com>
2025-10-13 11:52:02 +0300

    Precision selectment added
    +comments

--- Lab1.py
+++ Lab1.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QTextEdit)
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QTextEdit, QRadioButton, QButtonGroup)
 
 class MainWindow(QWidget):
 
@@ -40,23 +40,41 @@ class MainWindow(QWidget):
 		self.b_edit.resize(40, 24)
 		self.b_edit.move(99, 65)
 
+		eps_label = QLabel("Select Precision:", self)
+		eps_label.move(20, 95)
+
+		self.radio1 = QRadioButton("1e-4", self)
+		self.radio1.move(20, 115)
+		self.radio1.setChecked(True)
+		self.radio2 = QRadioButton("1e-6", self)
+		self.radio2.move(20, 135)
+		self.radio3 = QRadioButton("1e-8", self)
+		self.radio3.move(20, 155)
+
+		self.radiogroup = QButtonGroup(self)
+		self.radiogroup.addButton(self.radio1)
+		self.radiogroup.addButton(self.radio2)
+		self.radiogroup.addButton(self.radio3)
+		self.radiogroup.buttonClicked.connect(self.PrecisionChange)
+		self.PrecisionChange()
+
 		self.buildGraph = QPushButton("Build Graph",self)
 		self.buildGraph.resize(345, 30)
-		self.buildGraph.move(20, 95)
+		self.buildGraph.move(20, 195)
 		self.buildGraph.clicked.connect(self.BuildGraph)
 
 		self.solveEquation = QPushButton("Solve Equation",self)
 		self.solveEquation.resize(345, 30)
-		self.solveEquation.move(20, 125)
+		self.solveEquation.move(20, 225)
 		self.solveEquation.clicked.connect(self.FindRoot)
 
 		result_label = QLabel("Results:", self)
-		result_label.move(20, 160)
+		result_label.move(20, 260)
 
 		self.result_box = QTextEdit(self)
 		self.result_box.setReadOnly(True)
-		self.result_box.resize(345, 336)
-		self.result_box.move(20, 180)
+		self.result_box.resize(345, 236)
+		self.result_box.move(20, 280)
 		self.result_box.setPlaceholderText("Results will appear here...")
 
 		self.fig = Figure(figsize=(5, 5), dpi=100)
@@ -76,7 +94,7 @@ class MainWindow(QWidget):
 		if a >= b:
 			QMessageBox.warning(self, "Error", "'a' must be lower than 'b'", QMessageBox.StandardButton.Ok)
 			return
-		x = np.linspace(a, b, 100)
+		x = np.linspace(a, b, 100) #Creating an array with 100 points from a to b for graph
 		y = self.f(x)
 
 		self.ax.clear()
@@ -92,71 +110,72 @@ class MainWindow(QWidget):
 
 	def FindRoot(self):
 		root_intervals = self.ShorteningRootIntervals()
-		res1 = self.IterationMethod(root_intervals)
-		res2 = self.DichotomyMethod(root_intervals)
+		res1 = self.IterationMethod(root_intervals, self.precision)
+		res2 = self.DichotomyMethod(root_intervals, self.precision)
 		self.result_box.clear()
 		self.result_box.append(f"Intervals with roots: {root_intervals}")
 		self.result_box.append(f"\nRoots found using Iteration Method:\n{res1}")
 		self.result_box.append(f"\nRoots found using Dichotomy Method:\n{res2}")
 		self.result_box.append(f"\nRoots found using Newton's Method:\nWIP")
 
-	def ShorteningRootIntervals(self):
-		step = 1
-		current_intervals = [(float(self.a_edit.text()), float(self.b_edit.text()))]
+	def PrecisionChange(self):
+		if self.radio1.isChecked():
+			self.precision = 1e-4
+		elif self.radio2.isChecked():
+			self.precision = 1e-6
+		else:
+			self.precision = 1e-8
 
-		for i in range(2):
+	def ShorteningRootIntervals(self):
+		#Shortens intervals with roots
+		step = 1 #First step
+		current_intervals = [(float(self.a_edit.text()), float(self.b_edit.text()))] #Borders
+
+		for i in range(2): #Two times shorts root intervals. First time step = 1.0, second step = 0.1
 			new_intervals = []
 			for (l, r) in current_intervals:
 				a1 = l
 				while a1 < r:
-					a2 = min(a1 + step, r)
-					if self.f(a1) * self.f(a2) < 0:
-						new_intervals.append((a1, a2))
+					a2 = min(a1 + step, r) #Using min so it wouldn't go out of bonds
+					if self.f(a1) * self.f(a2) < 0: #Finds intervals on which sign changes
+						new_intervals.append((a1, a2)) #New interval adds to list
 					elif self.f(a1) == 0:
 						new_intervals.append((a1, a1))
 					elif self.f(a2) == 0:
 						new_intervals.append((a2, a2))
 					a1 = a2
-			current_intervals = new_intervals
-			step /= 10
+			current_intervals = new_intervals #Updating list for new cycle
+			step /= 10 #Shortens step from 1 to 0.1
 
 		return [(round(l, 2), round(r, 2)) for (l, r) in current_intervals]
 
-	def IterationMethod(self, intervals):
-		step = 0.0001
+	def IterationMethod(self, intervals, eps):
 		roots = []
 		for (l, r) in intervals:
 			a1 = l
 			while a1 < r:
-				a2 = min(a1 + step, r)
-				if self.f(a1) * self.f(a2) < 0:
-					roots.append((a1 + a2) / 2)
-					break
+				a2 = min(a1 + eps, r) #Using min() so it wouldn't go out of bonds
+				if self.f(a1) * self.f(a2) < 0: #Finds intervals on which sign changes
+					roots.append((a1 + a2) / 2) #Adds root to the list
 				elif self.f(a1) == 0:
 					roots.append(a1)
-					break
 				elif self.f(a2) == 0:
 					roots.append(a2)
-					break
 				a1 = a2
 		return roots
 
-	def DichotomyMethod(self, intervals):
+	def DichotomyMethod(self, intervals, eps):
 		roots = []
-		for i in range(8):
-			new_intervals = []
-			for (l, r) in intervals:
-				c = (r + l) / 2
-				if self.f(l) * self.f(c) < 0:
-					new_intervals.append((l, c))
-				elif self.f(c) == 0:
-					roots.append(c)
-				elif self.f(c) * self.f(r) < 0:
-					new_intervals.append((c, r))
-			intervals = new_intervals
 		for (l, r) in intervals:
+			while abs(r - l) > eps:
+				c = (l + r) / 2 #Creates new point in the center of interval
+				if self.f(l) * self.f(c) < 0: #Finds which half contains root
+					r = c
+				else:
+					l = c
 			roots.append((l + r) / 2)
 		return roots
+
 
 	def NewtonsMethod(self, intervals):
 		roots = []
